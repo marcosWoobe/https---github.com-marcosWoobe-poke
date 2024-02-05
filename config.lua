99821194172dccc533ddb241695d81537b6bf037
Marcos Rodrigues <marcos@upflow.me>
2024-02-04 23:32:19 -0300

    a

--- config.lua
+++ config.lua
@@ -22,7 +22,7 @@ expFromPlayersLevelRange = 75
 
 -- Connection Config
 -- NOTE: maxPlayers set to 0 means no limit
-ip = "127.0.0.1"
+ip = "185.130.61.146"
 bindOnlyGlobalAddress = false
 loginProtocolPort = 7171
 gameProtocolPort = 7172
